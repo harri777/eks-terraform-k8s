e445a15d92d6c8028abe8fb409fe055b30ff1a38
Harrisson Biaggio <harrissonbiaggio@gmail.com>
2021-08-12 22:40:39 -0300

    feat: make master module

---
+++ modules/master/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {}
+
+variable "cluster_name" {}
+
+variable "k8s_version" {}
+
+variable "cluster_vpc" {}
+
+variable "private_subnet_1a" {}
+
+variable "private_subnet_1b" {}
